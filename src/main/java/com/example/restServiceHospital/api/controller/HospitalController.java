d511f8f8b0b60aa77880c8b73f2cd654dc880520
Shehab El Din Adel <53188087+shehabadel@users.noreply.github.com>
2023-05-19 01:15:51 +0300

    Updated the hospitals endpoints

--- src/main/java/com/example/restServiceHospital/api/controller/HospitalController.java
+++ src/main/java/com/example/restServiceHospital/api/controller/HospitalController.java
@@ -4,34 +4,38 @@ package com.example.restServiceHospital.api.controller;
 import com.example.restServiceHospital.api.model.Hospital;
 import com.example.restServiceHospital.api.model.LocationDTO;
 import com.example.restServiceHospital.api.repository.HospitalRepository;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class HospitalController {
     @Autowired
     HospitalRepository hospitalRepository;
+    private static final Logger logger = LoggerFactory.getLogger(HospitalController.class);
 
-    @GetMapping("/hospital")
+    @GetMapping("/hospitals")
     public List<Hospital> getAll() {
         return (List<Hospital>) hospitalRepository.findAll();
     }
 
-    @GetMapping("/hospital/{id}")
+    @GetMapping("/hospitals/{id}")
     public Optional<Hospital> getByID(@PathVariable String id) {
         Integer ID = Integer.parseInt(id);
         return hospitalRepository.findById(ID);
 
     }
 
-    @PostMapping("/hospital/nearest")
+    @PostMapping("/hospitals/nearest")
     public Optional<Hospital> getByNearestHospital(@RequestBody LocationDTO location) {
         float hospitalLocationLat = location.getLatitude();
         float hospitalLocationLong = location.getLongitude();
-
+        logger.info("here");
         Hospital closestHospital = null;
         double minDistance = Double.MAX_VALUE;
         List<Hospital> hospitals = (List<Hospital>) hospitalRepository.findAvailableHospitals();
@@ -48,7 +52,7 @@ public class HospitalController {
         return Optional.ofNullable(closestHospital);
     }
 
-    @PostMapping("/hospital/confirm/{id}")
+    @PostMapping("/hospitals/confirm/{id}")
     public String confirmRequest(@PathVariable String id) {
         int rowsAffected = hospitalRepository.updateHospitalSeats(Integer.valueOf(id),hospitalRepository.findHospitalById(Integer.valueOf(id)).getNumberOfSeats()-1);
         if(rowsAffected > 0)
@@ -72,7 +76,7 @@ public class HospitalController {
     /**
      * Create a new driver
      */
-    @PostMapping("/hospital")
+    @PostMapping("/hospitals")
     public Hospital createHospital(@RequestBody Hospital newHospital) {
         return hospitalRepository.save(newHospital);
     }
